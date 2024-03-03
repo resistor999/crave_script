e85cf222eeb2b13dbc91b1496bb891b49033036d
darknight <darkniuslightness@gmail.com>
2024-03-04 05:32:10 +0700

    Update building.sh

--- building.sh
+++ building.sh
@@ -3,18 +3,13 @@ rm -rf .repo/local_manifests
 # Do repo init for rom that we want to build.
 repo init -u https://github.com/AOSPA/manifest -b topaz --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
 
-# Remove tree before cloning our manifest.
-rm -rf device/xiaomi
-rm -rf vendor/xiaomi
-rm -rf kernel/xiaomi
-
 # Do remove here before repo sync.
 rm -rf prebuilts/clang/host/linux-x86
-rm -rf hardware/qcom/display.git
-rm -rf hardware/qcom/gps
-rm -rf hardware/qcom/media
-rm -rf hardware/xiaomi
-rm -rf vendor/qcom/opensource/audio-hal/primary-hal
+rm -rf hardware
+rm -rf vendor
+rm -rf system
+rm -rf kernel
+rm -rf out/host
 rm -rf prebuilts/clang/host/linux-x86
 
 # Clone our local manifest.

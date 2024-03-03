a71e0ad9ad7f5d491c6711692c9c98b69a29a120
darknight <darkniuslightness@gmail.com>
2024-03-04 01:56:20 +0700

    Update building.sh

--- building.sh
+++ building.sh
@@ -1,18 +1,27 @@
 rm -rf .repo/local_manifests
 
 # Do repo init for rom that we want to build.
-repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b topaz -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/AOSPA/manifest -b topaz --git-lfs --depth=1 --no-repo-verify
 
 # Remove tree before cloning our manifest.
 rm -rf device/xiaomi
 rm -rf vendor/xiaomi
 rm -rf kernel/xiaomi
 
+# Do remove here before repo sync.
+rm -rf prebuilts/clang/host/linux-x86
+rm -rf hardware/qcom/display.git
+rm -rf hardware/qcom/gps
+rm -rf hardware/qcom/media
+rm -rf hardware/xiaomi
+rm -rf vendor/qcom/opensource/audio-hal/primary-hal
+rm -rf prebuilts/clang/host/linux-x86
+
 # Clone our local manifest.
 git clone https://github.com/Night-Raids-Reborn/local_manifest --depth 1 -b 13-topaz .repo/local_manifests
 
 # Let's sync!
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
     
 # Define timezone
 export TZ=Asia/Jakarta

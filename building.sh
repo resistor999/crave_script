62f580ba6f942cb94c6811b22b6a634a80e5d1af
darknight <darkniuslightness@gmail.com>
2024-03-05 14:43:42 +0700

    Update building.sh

--- building.sh
+++ building.sh
@@ -1,7 +1,7 @@
 rm -rf .repo/local_manifests
 
 # Do repo init for rom that we want to build.
-repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Evolution-X/manifest -b udc --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
 
 # Do remove here before repo sync.
 rm -rf hardware
@@ -20,11 +20,9 @@ git clone https://github.com/Night-Raids-Reborn/local_manifest --depth 1 -b usop
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 
 # Do remove here after repo sync.
-rm -rf hardware/xiaomi
 rm -rf hardware/samsung-ext/interfaces
 
 # Do clone here after repo sync.
-git clone https://github.com/Night-Raids-Reborn/hardware_xiaomi -b udc hardware/xiaomi
 git clone https://github.com/Roynas-Android-Playground/hardware_samsung-extra_interfaces -b master hardware/samsung-ext/interfaces
     
 # Define timezone
@@ -32,5 +30,5 @@ export TZ=Asia/Jakarta
 
 # Let's start build!
 . build/envsetup.sh
-lunch aosp_citrus-userdebug
-make bacon
+lunch evolution_citrus-userdebug
+m evolution

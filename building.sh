8ba997ad7b40fe681c1b0e81c98343a3b0a5bdac
darknight <darkniuslightness@gmail.com>
2024-03-06 08:13:59 +0700

    Update building.sh

--- building.sh
+++ building.sh
@@ -1,7 +1,7 @@
 rm -rf .repo/local_manifests
 
 # Do repo init for rom that we want to build.
-repo init -u https://github.com/Evolution-X/manifest -b udc --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Black-Iron-Project/manifest -b u14 --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
 
 # Do remove here before repo sync.
 rm -rf hardware
@@ -21,14 +21,16 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimiz
 
 # Do remove here after repo sync.
 rm -rf hardware/samsung-ext/interfaces
+rm -rf hardware/xiaomi
 
 # Do clone here after repo sync.
 git clone https://github.com/Roynas-Android-Playground/hardware_samsung-extra_interfaces -b master hardware/samsung-ext/interfaces
+git clone https://github.com/Night-Raids-Reborn/hardware_xiaomi -b udc hardware/xiaomi
     
 # Define timezone
 export TZ=Asia/Jakarta
 
 # Let's start build!
 . build/envsetup.sh
-lunch evolution_citrus-userdebug
-m evolution
+lunch blackiron_citrus-userdebug
+mka blackiron

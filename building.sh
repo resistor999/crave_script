643a2efd37f43bd69053afaa2b42b1bbc98c5b0c
darknight <darkniuslightness@gmail.com>
2024-03-10 10:11:12 +0700

    Update building.sh

--- building.sh
+++ building.sh
@@ -1,7 +1,7 @@
 rm -rf .repo/local_manifests
 
 # Do repo init for rom that we want to build.
-repo init -u https://github.com/CherishOS/android_manifest.git -b udc --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/Night-Raids-Reborn/manifest-rom -b havoc-11 --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
 
 # Do remove here before repo sync.
 rm -rf hardware
@@ -13,26 +13,13 @@ rm -rf packages
 rm -rf prebuilts/clang/host/linux-x86
 rm -rf out/host
 
-# Clone our local manifest.
-git clone https://github.com/Night-Raids-Reborn/local_manifest --depth 1 -b u .repo/local_manifests
-
 # Let's sync!
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
-
-# Do remove here after repo sync.
-rm -rf hardware/xiaomi
-rm -rf packages/resources/devicesettings
-rm -rf system/libhidl
-
-# Do clone here after repo sync.
-git clone https://github.com/Night-Raids-Reborn/hardware_xiaomi -b udc hardware/xiaomi
-git clone https://github.com/PixelExperience/packages_resources_devicesettings -b fourteen packages/resources/devicesettings
-git clone https://github.com/Evolution-X/system_libhidl -b udc system/libhidl
     
 # Define timezone
 export TZ=Asia/Jakarta
 
 # Let's start build!
 . build/envsetup.sh
-lunch cherish_citrus-userdebug
-mka bacon
+lunch havoc_chime-userdebug
+make bacon

d5b9aed01fad1470906c4362e0414db6b4747266
resistor999 <162920326+resistor999@users.noreply.github.com>
2024-03-13 02:20:26 +0700

    Update building.sh

--- building.sh
+++ building.sh
@@ -1,16 +1,37 @@
 rm -rf .repo/local_manifests
 
 # Do repo init for rom that we want to build.
-repo init -u https://github.com/AOSPA/manifest -b topaz --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/AfterLifePrjkt13/android_manifest.git -b LTS --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
+
+# Do remove here before repo sync.
+rm -rf hardware
+rm -rf vendor
+rm -rf system
+rm -rf kernel
+rm -rf device
+rm -rf packages
+rm -rf prebuilts/clang/host/linux-x86
+rm -rf prebuilts
+rm -rf out/host
 
 # Clone our local manifest.
-git clone https://github.com/Night-Raids-Reborn/local_manifest --depth 1 -b 13-topaz .repo/local_manifests
+git clone https://github.com/Night-Raids-Reborn/local_manifest --depth 1 -b 13-n .repo/local_manifests
 
 # Let's sync!
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
+
+# Do remove here after repo sync.
+rm -rf packages/resources/devicesettings
+rm -rf vendor/qcom/opensource/vibrator
+
+# Do clone here after repo sync.
+git clone https://github.com/PixelExperience/packages_resources_devicesettings -b thirteen packages/resources/devicesettings
+git clone https://github.com/Night-Raids-Reborn/android_vendor_qcom_opensource_vibrator -b thirteen vendor/qcom/opensource/vibrator
     
 # Define timezone
 export TZ=Asia/Jakarta
 
 # Let's start build!
-bash rom-build.sh citrus -t userdebug -i
+. build/envsetup.sh
+lunch afterlife_citrus-userdebug
+m afterlife

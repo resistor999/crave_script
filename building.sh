72e7b285f18d80fa2edb7113901bb85603107472
resistor999 <162920326+resistor999@users.noreply.github.com>
2024-03-14 02:54:16 +0700

    Update building.sh

--- building.sh
+++ building.sh
@@ -1,7 +1,7 @@
 rm -rf .repo/local_manifests
 
 # Do repo init for rom that we want to build.
-repo init -u https://github.com/AfterLifePrjkt13/android_manifest.git -b LTS --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/BuildBots-Den/manifest_spark -b pyro-next --depth=1 --no-repo-verify -g default,-mips,-darwin,-notdefault
 
 # Do remove here before repo sync.
 rm -rf hardware
@@ -15,7 +15,7 @@ rm -rf prebuilts
 rm -rf out/host
 
 # Clone our local manifest.
-git clone https://github.com/resistor999/local_manifest --depth 1 -b 13 .repo/local_manifests
+git clone https://github.com/Night-Raids-Reborn/local_manifest --depth 1 -b 13-n .repo/local_manifests
 
 # Let's sync!
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
@@ -23,17 +23,19 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimiz
 # Do remove here after repo sync.
 rm -rf packages/resources/devicesettings
 rm -rf vendor/qcom/opensource/vibrator
-rm -rf vendor/signed
+rm -rf packages/providers/DownloadProvider
+rm -rf hardware/xiaomi
 
 # Do clone here after repo sync.
 git clone https://github.com/PixelExperience/packages_resources_devicesettings -b thirteen packages/resources/devicesettings
 git clone https://github.com/Night-Raids-Reborn/android_vendor_qcom_opensource_vibrator -b thirteen vendor/qcom/opensource/vibrator
-git clone https://github.com/resistor999/vendro_al -b 13 vendor/signed
+git clone https://github.com/ArrowOS/android_packages_providers_DownloadProvider -b arrow-13.1 packages/providers/DownloadProvider
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
     
 # Define timezone
 export TZ=Asia/Jakarta
 
 # Let's start build!
 . build/envsetup.sh
-lunch afterlife_citrus-userdebug
-m afterlife
+lunch spark_citrus-userdebug
+mka bacon
